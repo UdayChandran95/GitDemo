f80dc08e68451932f19cd6697a6176a049721c70
UdayChandran95 <udayachandran13@gmail.com>
2022-08-25 09:20:12 -1200

    first commit

---
+++ src/Apllication.java
@@ -0,0 +1,15 @@
+
+public class Apllication {
+	
+	public static void main(String[] args) {
+		int aNumber = 5;
+		double aDouble = 5.6;
+		String text = "hello";
+		String blank = " ";
+		String moreText = "Peter";
+		System.out.println(text + blank + moreText);
+		System.out.println(text);
+		System.out.println("My numbers:" + aNumber + " , " + aDouble);
+	}
+
+}
